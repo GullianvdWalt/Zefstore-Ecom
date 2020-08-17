4ae89f030519bb561f2da71024cc3244faa8d7a5
GullianvdWalt <51962834+GullianvdWalt@users.noreply.github.com>
2020-08-17 06:30:03 +0200

    Changed Search bar

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -1,4 +1,3 @@
 <form action="{{ route('search') }}" method="GET" class="search-form">
-    <i class="fa fa-search search-icon"></i>
     <input type="text" name="query" id="query" value="{{ request()->input('query') }}" class="search-box" placeholder="Search for product" required>
 </form>
